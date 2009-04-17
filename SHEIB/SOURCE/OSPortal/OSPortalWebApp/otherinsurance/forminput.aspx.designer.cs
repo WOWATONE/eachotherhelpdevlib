c8bc3415530b3f4daf7c9b4f39cd342d7d69ae0b
lmc309 <lmc309@228527cc-2361-11de-8096-4f48778bd5c7>
2009-04-17 07:44:37 +0000

    add gridview.

--- SHEIB/SOURCE/OSPortal/OSPortalWebApp/otherinsurance/forminput.aspx.designer.cs
+++ SHEIB/SOURCE/OSPortal/OSPortalWebApp/otherinsurance/forminput.aspx.designer.cs
@@ -14,6 +14,15 @@ namespace OSPortalWebApp.otherinsurance {
     public partial class forminput {
         
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ToolkitScriptManager ScriptManager1;
+        
+        /// <summary>
         /// carTabPage control.
         /// </summary>
         /// <remarks>
