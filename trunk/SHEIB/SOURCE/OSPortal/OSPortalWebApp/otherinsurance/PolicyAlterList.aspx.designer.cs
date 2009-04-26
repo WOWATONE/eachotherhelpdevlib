7a2f049c12ded489c4a340861e4dedb665c1c204
lmc309 <lmc309@228527cc-2361-11de-8096-4f48778bd5c7>
2009-04-26 07:46:01 +0000

    add files

--- trunk/SHEIB/SOURCE/OSPortal/OSPortalWebApp/otherinsurance/PolicyAlterList.aspx.designer.cs
+++ trunk/SHEIB/SOURCE/OSPortal/OSPortalWebApp/otherinsurance/PolicyAlterList.aspx.designer.cs
@@ -23,6 +23,15 @@ namespace OSPortalWebApp.otherinsurance {
         protected global::AjaxControlToolkit.ToolkitScriptManager ScriptManager1;
         
         /// <summary>
+        /// dpASPxMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxMenu.ASPxMenu dpASPxMenu;
+        
+        /// <summary>
         /// npSearchHeader control.
         /// </summary>
         /// <remarks>
