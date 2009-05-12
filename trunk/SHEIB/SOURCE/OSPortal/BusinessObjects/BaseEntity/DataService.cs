0354c8c2cc04df9945ee124d43574e4aa029e479
lmc309 <lmc309@228527cc-2361-11de-8096-4f48778bd5c7>
2009-05-12 10:34:51 +0000

    add business objects

---
+++ trunk/SHEIB/SOURCE/OSPortal/BusinessObjects/BaseEntity/DataService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Xml;
+using System.Data.Common;
+using Microsoft.Practices.EnterpriseLibrary.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+
+namespace BusinessObjects
+{
+    public class DataService
+    {
+        public static readonly Database DB = DatabaseFactory.CreateDatabase();        
+    }
+}
