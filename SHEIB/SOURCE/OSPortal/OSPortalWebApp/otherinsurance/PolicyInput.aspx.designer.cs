56ecb75aabae33777d7ee16f71acff25a5005209
lmc309 <lmc309@228527cc-2361-11de-8096-4f48778bd5c7>
2009-05-16 06:28:13 +0000

    FeeNotice

--- SHEIB/SOURCE/OSPortal/OSPortalWebApp/otherinsurance/PolicyInput.aspx.designer.cs
+++ SHEIB/SOURCE/OSPortal/OSPortalWebApp/otherinsurance/PolicyInput.aspx.designer.cs
@@ -527,6 +527,24 @@ namespace OSPortalWebApp.otherinsurance {
         protected global::DevExpress.Web.ASPxClasses.ContentControl ContentControl1;
         
         /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// deCheckDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxEditors.ASPxDateEdit deCheckDate;
+        
+        /// <summary>
         /// taModifyMemo control.
         /// </summary>
         /// <remarks>
