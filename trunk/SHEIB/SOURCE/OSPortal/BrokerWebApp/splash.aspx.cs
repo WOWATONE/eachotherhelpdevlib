96dd49cf242ec35ba8ec0e8dff5941d0a2dc905e
lmc309 <lmc309@228527cc-2361-11de-8096-4f48778bd5c7>
2009-05-20 12:37:21 +0000

    fix bugs

--- trunk/SHEIB/SOURCE/OSPortal/BrokerWebApp/splash.aspx.cs
+++ trunk/SHEIB/SOURCE/OSPortal/BrokerWebApp/splash.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BusinessObjects;
 
 namespace BrokerWebApp
 {
@@ -11,7 +12,7 @@ namespace BrokerWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //List<BO_P_User> datalist = BO_P_User.FetchList();
         }
     }
 }
