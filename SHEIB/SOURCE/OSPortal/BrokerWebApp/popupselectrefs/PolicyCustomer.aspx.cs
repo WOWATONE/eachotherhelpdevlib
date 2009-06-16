8b99e3fff901cea42c4d1626e4132afc5ee6bd87
lmc309 <lmc309@228527cc-2361-11de-8096-4f48778bd5c7>
2009-06-16 09:58:29 +0000

    add policy customer reference.

---
+++ SHEIB/SOURCE/OSPortal/BrokerWebApp/popupselectrefs/PolicyCustomer.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BrokerWebApp.popupselectrefs
+{
+    public partial class PolicyCustomer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        protected void gridSearchResult_DataBinding(object sender, EventArgs e)
+        {
+            //
+        }
+
+        protected void gridSearchResult_HtmlRowCreated(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewTableRowEventArgs e)
+        {
+            //FindControl("");
+            //
+        }
+
+        protected void gridSearchResult_PageIndexChanged(object sender, EventArgs e)
+        {
+            //this.gridSearchResult.PageIndex;
+            //this.gridSearchResult.VisibleRowCount;
+            //this.gridSearchResult.VisibleStartIndex;
+        }
+
+    }
+}
