686bb50c1a35b4d4decd9b76fdb0164395e5ee4a
lmc309 <lmc309@228527cc-2361-11de-8096-4f48778bd5c7>
2009-08-07 11:55:16 +0000

    modify.

--- SHEIB/SOURCE/OSPortal/BrokerWebApp/PolicyViewTransfer.aspx.cs
+++ SHEIB/SOURCE/OSPortal/BrokerWebApp/PolicyViewTransfer.aspx.cs
@@ -9,10 +9,24 @@ namespace BrokerWebApp
 {
     public partial class PolicyViewTransfer : System.Web.UI.Page
     {
+
+        #region Variables
+
+        private const string inputQueryStringIDKey = "id";
+
+        #endregion Variables
+
+
+        #region Page Events
+
         protected void Page_Load(object sender, EventArgs e)
         {
             Page.Response.Redirect("otherinsurance/PolicyView.aspx?id=");
             Page.Response.Redirect("vehicleinsurance/CarPolicyView.aspx?id=");
         }
+
+        #endregion Page Events
+
+
     }
 }
